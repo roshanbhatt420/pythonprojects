00135a8e49d50e1580523a047139d44dbe0597d5
roshanbhatta420 <roshanbhatta605@gmail.com>
2024-11-14 21:27:17 +0545

    just about numpy

---
+++ numpyone.py
@@ -0,0 +1,17 @@
+# things you have to know before start 
+# what is NUmpy
+# why Numpy 
+# NUmpy is pakage
+# Numpy is used for numerical operation
+# Numpy is used for scientific operation
+# Numpy is used for mathematical operation
+# Numpy is used for data analysis
+# Numpy is used for data manipulation
+# Numpy is used for data visualization
+# Numpy is used for data cleaning
+# Numpy is used for data transformation
+# Numpy is used for data wrangling
+# Numpy is used for data preprocessing
+# Numpy is used for data mining
+# Numpy is used for data modeling
+# Numpy is used for data prediction
